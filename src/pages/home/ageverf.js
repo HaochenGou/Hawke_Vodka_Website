a2f7e9d5f611d5a728308fba2c6dc32c155de77d
Haochen Gou <hgou@ualberta.ca>
2022-03-03 12:24:13 -0700

    Update

--- src/pages/home/ageverf.js
+++ src/pages/home/ageverf.js
@@ -1,8 +1,33 @@
 import PropTypes from "prop-types";
-import React from "react";
+import React, { Component } from "react";
 
+export default class PopUp extends Component {
+  handleClick = () => {
+    this.props.toggle();
+  };
 
-const ageverf = () => {};
+  render() {
+    return (
+      <div className="modal">
+        <div className="modal_content">
+          <span className="close" onClick={this.handleClick}>
+            &times;
+          </span>
+          <form>
+            <h3>Register!</h3>
+            <label>
+              Name:
+              <input type="text" name="name" />
+            </label>
+            <br />
+            <input type="submit" />
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
 
-
-export default ageverf;
+PopUp.propTypes = {
+    location: PropTypes.object
+  };
