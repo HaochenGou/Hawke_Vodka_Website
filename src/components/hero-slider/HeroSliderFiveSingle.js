f6cef53df223917c604982a90e0ff62c3f846667
Haochen Gou <hgou@ualberta.ca>
2022-03-04 11:08:48 -0700

    Update

--- src/components/hero-slider/HeroSliderFiveSingle.js
+++ src/components/hero-slider/HeroSliderFiveSingle.js
@@ -11,10 +11,6 @@ const HeroSliderFiveSingle = () => {
       src="/assets/img/slider/slider-1.webp"
       alt="First slide"
     />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
     <img
@@ -23,10 +19,6 @@ const HeroSliderFiveSingle = () => {
       alt="Second slide"
     />
 
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
     <img
@@ -35,10 +27,6 @@ const HeroSliderFiveSingle = () => {
       alt="Third slide"
     />
 
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
   </Carousel.Item>
 </Carousel>
     
