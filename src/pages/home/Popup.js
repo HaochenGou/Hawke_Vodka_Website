a041a6e30492fbc63a8c738bccc8fb75658842a7
Haochen Gou <hgou@ualberta.ca>
2022-03-04 09:29:20 -0700

    Update

--- src/pages/home/Popup.js
+++ src/pages/home/Popup.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import Popup from 'reactjs-popup';
+import React, {useState, useEffect} from "react";
+import "./Popup.css"
+import { Button, Modal} from "@mui/material";
 
-const PopupExample = () => (
-  <Popup trigger={<button> Trigger</button>} position="right center">
-    <div>Popup content here !!</div>
-  </Popup>
-);
+
+const Popup = (props) => {
+  const[isVerify, setIsVerify] = useState(false)
+  useEffect(() =>{localStorage.setItem("verify",JSON.stringify(isVerify));},[isVerify]);
+
+  return (
+    <Modal
+    onClose={false}
+    aria-labelledby="simple-modal-title"
+    aria-describedby="simple-modal-description"
+  >
+    <div className="popup-box">
+      <div className="box">
+        <Button variant="contained" size="small" onClick={true}>Agree</Button>{' '}
+        <Button variant="contained" size="small" href = "https://www.drinksmart.com/">Disagree</Button>
+
+        {props.content}
+      </div>
+    </div>
+    </Modal>
+  )
+}
+
+export default Popup
