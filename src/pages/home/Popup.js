ff8764db0b5640fff4c960ff9df9c07df45b6ef0
Haochen Gou <hgou@ualberta.ca>
2022-03-07 11:58:12 -0700

    Update

--- src/pages/home/Popup.js
+++ src/pages/home/Popup.js
@@ -10,10 +10,6 @@ const Popup = (props) => {
   const toggle=()=>{
     setIsVerify(!verify);
   }
-
-
-  
-
   return (
     <div>
     {toggle &&
@@ -21,7 +17,7 @@ const Popup = (props) => {
         <div className="box">
           <b>Are you of legal drinking alcohol age?</b>
           <p>By click the Yes button, you certify that you are of legal drinking alcohol age in the state in which you reside</p>
-          <Button variant="contained" size="small" onClick={props.handleClose}>Yes</Button>{' '}
+          <Button variant="contained" size="small" onClick={}>Yes</Button>{' '}
           <Button variant="contained" size="small" href = "https://www.drinksmart.com/">No</Button>
         </div>
       </div>
