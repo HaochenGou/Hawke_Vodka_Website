d91513a98e67e6a167e0e4c314c1a6d2541371cf
Haochen Gou <hgou@ualberta.ca>
2022-03-07 13:45:23 -0700

    Update

--- src/pages/home/Popup.js
+++ src/pages/home/Popup.js
@@ -1,27 +1,28 @@
 import React, {useState, useEffect} from "react";
 import "./Popup.css"
-import { Button} from "@mui/material";
+
+
+
 
 
 const Popup = (props) => {
-  const[isVerify, setIsVerify] = useState(false)
+  const[isVerify, setIsVerify] = useState(false);
   useEffect(() =>{localStorage.setItem("verify",JSON.stringify('verify'));},[isVerify]);
   const verify = localStorage.getItem("verify");
   const toggle=()=>{
-    setIsVerify(!verify);
+    setIsVerify(true);
   }
+ 
+ 
   return (
     <div>
-    {toggle &&
-      (<div className="popup-box">
-        <div className="box">
-          <b>Are you of legal drinking alcohol age?</b>
-          <p>By click the Yes button, you certify that you are of legal drinking alcohol age in the state in which you reside</p>
-          <Button variant="contained" size="small" onClick={}>Yes</Button>{' '}
-          <Button variant="contained" size="small" href = "https://www.drinksmart.com/">No</Button>
-        </div>
-      </div>
-      )}
+    {verify &&
+      <Popup
+      content={<>
+        <b>Are you of legal drinking alcohol age?</b>
+        <p>By click the Yes button, you certify that you are of legal drinking alcohol age in the state in which you reside</p>
+      </>}
+      handleClose={toggle}/>}
     </div>
   )
 }
