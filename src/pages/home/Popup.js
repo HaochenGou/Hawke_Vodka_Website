f8dcd15cf1d558196d8bbc998fd9ab0326066eb2
Haochen Gou <hgou@ualberta.ca>
2022-03-08 11:26:19 -0700

    Update

--- src/pages/home/Popup.js
+++ src/pages/home/Popup.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react";
 import "./Popup.css"
 import { Button, Stack} from "@mui/material";
+import Logo from "../../components/header/Logo";
 
 
 
@@ -22,9 +23,16 @@ const Popup = (props) => {
     (<div id = 'pop' style={{visibility:'visible'}}>
       <div className="popup-box">
         <div className="box">
+        <b className="brand">Hawke Prohibition Distilleries</b><br></br>
+        <p style={{margin: "20px"}}></p>
+        <Logo
+                imageUrl="/assets/img/logo/logo.webp"
+                logoClass="logo-hm-9"
+          />
+          <p style={{margin: "20px"}}></p>
           <b className="title">Are you of legal drinking alcohol age?</b><br></br>
           <p style={{margin: "40px"}}></p>
-          <Stack justifyContent="center" direction="row" spacing={10} alignItems="center">
+          <Stack justifyContent="center" direction="row" spacing={5} alignItems="center">
           <Button variant="contained" size="large" onClick={toggle}>Yes</Button>
           <Button variant="contained" size="large" href = "https://www.drinksmart.com/">No</Button><br></br>
           </Stack>
