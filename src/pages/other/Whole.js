955f9cee17772a21a0f5b513667e53602b31b205
Haochen Gou <hgou@ualberta.ca>
2022-03-08 15:41:38 -0700

    Update

--- src/pages/other/Whole.js
+++ src/pages/other/Whole.js
@@ -5,6 +5,7 @@ import { BreadcrumbsItem } from "react-breadcrumbs-dynamic";
 import LayoutOne from "../../layouts/LayoutOne";
 import Breadcrumb from "../../wrappers/breadcrumb/Breadcrumb";
 import BrandLogoSliderOne from "../../wrappers/brand-logo/BrandLogoSliderOne";
+import LocationMap from "../../components/contact/LocationMap";
 
 
 const Wholesale = ({ location }) => {
@@ -27,6 +28,105 @@ const Wholesale = ({ location }) => {
       <LayoutOne headerTop="visible">
         {/* breadcrumb */}
         <Breadcrumb />
+        <div className="contact-area pt-100 pb-100">
+          <div className="container">
+            <div className="contact-map mb-10">
+              <LocationMap latitude="53.536850" longitude="-113.331281" />
+            </div>
+            <div className="custom-row-2">
+              <div className="col-lg-4 col-md-5">
+                <div className="contact-info-wrap">
+                  <div className="single-contact-info">
+                    <div className="contact-icon">
+                      <i className="fa fa-phone" />
+                    </div>
+                    <div className="contact-info-dec">
+                      <p>+1 780-449-5331</p>
+                      <p>+1 780-449-5331</p>
+                    </div>
+                  </div>
+                  <div className="single-contact-info">
+                    <div className="contact-icon">
+                      <i className="fa fa-globe" />
+                    </div>
+                    <div className="contact-info-dec">
+                      <p>
+                        <a href="mailto:urname@email.com">info@hawkepro.com</a>
+                      </p>
+                      <p>
+                        <a href="//urwebsitenaem.com">info@hawkepro.com</a>
+                      </p>
+                    </div>
+                  </div>
+                  <div className="single-contact-info">
+                    <div className="contact-icon">
+                      <i className="fa fa-map-marker" />
+                    </div>
+                    <div className="contact-info-dec">
+                      <p>426 Kaska Road,  </p>
+                      <p>Sherwood Park, Alberta T8A 4G8.</p>
+                    </div>
+                  </div>
+                  <div className="contact-social text-center">
+                    <h3>Follow Us</h3>
+                    <ul>
+                      <li>
+                        <a href="//facebook.com">
+                          <i className="fa fa-facebook" />
+                        </a>
+                      </li>
+                      <li>
+                        <a href="//pinterest.com">
+                          <i className="fa fa-pinterest-p" />
+                        </a>
+                      </li>
+                      <li>
+                        <a href="//twitter.com">
+                          <i className="fa fa-twitter" />
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-8 col-md-7">
+                <div className="contact-form">
+                  <div className="contact-title mb-30">
+                    <h2>Get In Touch</h2>
+                  </div>
+                  <form className="contact-form-style">
+                    <div className="row">
+                      <div className="col-lg-6">
+                        <input name="name" placeholder="Name*" type="text" />
+                      </div>
+                      <div className="col-lg-6">
+                        <input name="email" placeholder="Email*" type="email" />
+                      </div>
+                      <div className="col-lg-12">
+                        <input
+                          name="subject"
+                          placeholder="Subject*"
+                          type="text"
+                        />
+                      </div>
+                      <div className="col-lg-12">
+                        <textarea
+                          name="message"
+                          placeholder="Your Massege*"
+                          defaultValue={""}
+                        />
+                        <button className="submit" type="submit">
+                          SEND
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                  <p className="form-messege" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
        
         {/* brand logo slider */}
         <BrandLogoSliderOne spaceBottomClass="pb-70" />
