71154c4fd1e65b971089d3212146033a453df82e
Haochen Gou <hgou@ualberta.ca>
2022-03-08 14:42:10 -0700

    Update

--- src/pages/home/Popup.js
+++ src/pages/home/Popup.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import "./Popup.css"
 import { Button, Stack} from "@mui/material";
 import Logo from "../../components/header/Logo";
