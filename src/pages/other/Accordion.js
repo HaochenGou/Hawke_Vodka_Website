2cfc41f941659e66568c925852150f7c93be4e76
Haochen Gou <hgou@ualberta.ca>
2022-03-09 10:44:41 -0700

    Update

--- src/pages/other/Accordion.js
+++ src/pages/other/Accordion.js
@@ -78,11 +78,7 @@ export default function SimpleAccordion() {
           aria-controls="panel4a-content"
           id="panel4a-header"
         >
-          <ReactPlayer url='https://www.youtube.com/watch?v=q0hyYWKXF0Q' 
-            playing = {true}
-            muted = {true}
-            width = '400px'
-            height='380px'/>
+          <img src="assets/img/recipes/4.webp" alt="recipes"/>
             <p style={{margin: "100px"}}></p>
             <Typography fontSize="20px">Ingredients</Typography>
         </AccordionSummary>
@@ -103,11 +99,7 @@ export default function SimpleAccordion() {
           aria-controls="panel5a-content"
           id="panel5a-header"
         >
-            <ReactPlayer url='https://www.youtube.com/watch?v=q0hyYWKXF0Q' 
-            playing = {true}
-            muted = {true}
-            width = '400px'
-            height='380px'/>
+            <img src="assets/img/recipes/5.webp" alt="recipes"/>
             <p style={{margin: "100px"}}></p>
             <Typography fontSize="20px">Ingredients</Typography>
         </AccordionSummary>
@@ -128,11 +120,7 @@ export default function SimpleAccordion() {
           aria-controls="panel6a-content"
           id="panel6a-header"
         >
-          <ReactPlayer url='https://www.youtube.com/watch?v=q0hyYWKXF0Q' 
-            playing = {true}
-            muted = {true}
-            width = '400px'
-            height='380px'/>
+          <img src="assets/img/recipes/6.webp" alt="recipes"/>
             <p style={{margin: "100px"}}></p>
             <Typography fontSize="20px">Ingredients</Typography>
         </AccordionSummary>
@@ -153,11 +141,7 @@ export default function SimpleAccordion() {
           aria-controls="panel7a-content"
           id="panel7a-header"
         >
-          <ReactPlayer url='https://www.youtube.com/watch?v=q0hyYWKXF0Q' 
-            playing = {true}
-            muted = {true}
-            width = '400px'
-            height='380px'/>
+          <img src="assets/img/recipes/7.webp" alt="recipes"/>
             <p style={{margin: "100px"}}></p>
             <Typography fontSize="20px">Ingredients</Typography>
         </AccordionSummary>
@@ -178,11 +162,7 @@ export default function SimpleAccordion() {
           aria-controls="panel8a-content"
           id="panel8a-header"
         >
-          <ReactPlayer url='https://www.youtube.com/watch?v=q0hyYWKXF0Q' 
-            playing = {true}
-            muted = {true}
-            width = '400px'
-            height='380px'/>
+          <img src="assets/img/recipes/8.webp" alt="recipes"/>
             <p style={{margin: "100px"}}></p>
             <Typography fontSize="20px">Ingredients</Typography>
         </AccordionSummary>
@@ -193,103 +173,6 @@ export default function SimpleAccordion() {
           </Typography>
         </AccordionDetails>
       </Accordion>
-
-      <p style={{margin: "100px"}}></p>
-
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel9a-content"
-          id="panel9a-header"
-        >
-          <ReactPlayer url='https://www.youtube.com/watch?v=q0hyYWKXF0Q' 
-            playing = {true}
-            muted = {true}
-            width = '400px'
-            height='380px'/>
-            <p style={{margin: "100px"}}></p>
-            <Typography fontSize="20px">Ingredients</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-
-      <p style={{margin: "100px"}}></p>
-
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel10a-content"
-          id="panel10a-header"
-        >
-          <ReactPlayer url='https://www.youtube.com/watch?v=q0hyYWKXF0Q' 
-            playing = {true}
-            muted = {true}
-            width = '400px'
-            height='380px'/>
-            <p style={{margin: "100px"}}></p>
-            <Typography fontSize="20px">Ingredients</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-
-      <p style={{margin: "100px"}}></p>
-
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel11a-content"
-          id="panel11a-header"
-        >
-          <ReactPlayer url='https://www.youtube.com/watch?v=q0hyYWKXF0Q' 
-            playing = {true}
-            muted = {true}
-            width = '400px'
-            height='380px'/>
-            <p style={{margin: "100px"}}></p>
-            <Typography fontSize="20px">Ingredients</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-
-      <p style={{margin: "100px"}}></p>
-
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel12a-content"
-          id="panel12a-header"
-        >
-          <ReactPlayer url='https://www.youtube.com/watch?v=q0hyYWKXF0Q' 
-            playing = {true}
-            muted = {true}
-            width = '400px'
-            height='380px'/>
-            <p style={{margin: "100px"}}></p>
-            <Typography fontSize="20px">Ingredients</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-
       <p style={{margin: "100px"}}></p>
      
     </div>
