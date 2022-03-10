28c002c3a95ac74ac2b26f14ff713f1f4fbe7ead
Haochen Gou <hgou@ualberta.ca>
2022-03-10 10:07:06 -0700

    Update

--- src/pages/other/quickbuy.js
+++ src/pages/other/quickbuy.js
@@ -5,13 +5,32 @@ import { BreadcrumbsItem } from "react-breadcrumbs-dynamic";
 import LayoutOne from "../../layouts/LayoutOne";
 import Breadcrumb from "../../wrappers/breadcrumb/Breadcrumb";
 import BrandLogoSliderOne from "../../wrappers/brand-logo/BrandLogoSliderOne";
+import ShopifyBuy from "shopify-buy";
 
 
 
 
 const Buy = ({ location }) => {
   const { pathname } = location;
-
+  var client = ShopifyBuy.buildClient({
+    domain: 'baby-x-vodka.myshopify.com',
+    apiKey: '97b7d775564daddb58a8aca065adcf83',
+  });
+  
+  var ui = ShopifyBuy.UI.init(client);
+  
+  var productNodes = document.querySelectorAll('[data-shopify-product]');
+  
+  Array.prototype.forEach.call(productNodes, function(node) {
+    var productId = node.dataset.shopifyProduct;
+    
+  
+  ui.createComponent('collection', {
+    id: 283202879639,
+    
+  });
+  
+ 
   return (
     <Fragment>
       <MetaTags>
@@ -29,110 +48,7 @@ const Buy = ({ location }) => {
       <LayoutOne headerTop="visible">
         {/* breadcrumb */}
         <Breadcrumb />
-        <div id='collection-component-1646868070989'></div>
-<script type="text/javascript">
-/*<![CDATA[*/
-(function () {
-  var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
-  if (window.ShopifyBuy) {
-    if (window.ShopifyBuy.UI) {
-      ShopifyBuyInit();
-    } else {
-      loadScript();
-    }
-  } else {
-    loadScript();
-  }
-  function loadScript() {
-    var script = document.createElement('script');
-    script.async = true;
-    script.src = scriptURL;
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
-    script.onload = ShopifyBuyInit;
-  }
-  function ShopifyBuyInit() {
-    var client = ShopifyBuy.buildClient({
-      domain: 'baby-x-vodka.myshopify.com',
-      storefrontAccessToken: '97b7d775564daddb58a8aca065adcf83',
-    });
-    ShopifyBuy.UI.onReady(client).then(function (ui) {
-      ui.createComponent('collection', {
-        id: '283202879639',
-        node: document.getElementById('collection-component-1646868070989'),
-        moneyFormat: '%24%7B%7Bamount%7D%7D',
-        options: {
-  "product": {
-    "styles": {
-      "product": {
-        "@media (min-width: 601px)": {
-          "max-width": "calc(25% - 20px)",
-          "margin-left": "20px",
-          "margin-bottom": "50px",
-          "width": "calc(25% - 20px)"
-        },
-        "img": {
-          "height": "calc(100% - 15px)",
-          "position": "absolute",
-          "left": "0",
-          "right": "0",
-          "top": "0"
-        },
-        "imgWrapper": {
-          "padding-top": "calc(75% + 15px)",
-          "position": "relative",
-          "height": "0"
-        }
-      }
-    },
-    "text": {
-      "button": "Add to cart"
-    }
-  },
-  "productSet": {
-    "styles": {
-      "products": {
-        "@media (min-width: 601px)": {
-          "margin-left": "-20px"
-        }
-      }
-    }
-  },
-  "modalProduct": {
-    "contents": {
-      "img": false,
-      "imgWithCarousel": true,
-      "button": false,
-      "buttonWithQuantity": true
-    },
-    "styles": {
-      "product": {
-        "@media (min-width: 601px)": {
-          "max-width": "100%",
-          "margin-left": "0px",
-          "margin-bottom": "0px"
-        }
-      }
-    },
-    "text": {
-      "button": "Add to cart"
-    }
-  },
-  "option": {},
-  "cart": {
-    "text": {
-      "total": "Subtotal",
-      "button": "Checkout"
-    },
-    "popup": false
-  },
-  "toggle": {}
-},
-      });
-    });
-  }
-})();
-/*]]>*/
-</script>
+        
         
        
         {/* brand logo slider */}
