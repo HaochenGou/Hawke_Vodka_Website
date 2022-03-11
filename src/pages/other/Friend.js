66e7e7352e6dea648ad9e449b9ff4ccf0bafcc13
Haochen Gou <hgou@ualberta.ca>
2022-03-11 13:12:08 -0700

    Update

--- src/pages/other/Friend.js
+++ src/pages/other/Friend.js
@@ -5,6 +5,20 @@ import { BreadcrumbsItem } from "react-breadcrumbs-dynamic";
 import LayoutOne from "../../layouts/LayoutOne";
 import Breadcrumb from "../../wrappers/breadcrumb/Breadcrumb";
 import BrandLogoSliderOne from "../../wrappers/brand-logo/BrandLogoSliderOne";
+import "./Friend.css";
+import { Stack } from "@mui/material";
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+
 
 
 const Friend = ({ location }) => {
@@ -27,8 +41,76 @@ const Friend = ({ location }) => {
       <LayoutOne headerTop="visible">
         {/* breadcrumb */}
         <Breadcrumb />
+        <p style={{margin: "100px"}}></p>
+
+        <Stack direction="row" spacing={15}>
+        <p style={{margin: "100px"}}></p>
+          <Item>
+          <div class="canvas-wrapper">
+            <a href="http://www.crossroadsliquorstore.com/" class="canvas" rel="noopener noreferrer"
+              target="_blank">
+              <div class="canvas_border">
+                <svg>
+                  <defs><linearGradient id="grad-orange" x1="0%" y1="0%" x2="100%" y2="0%"><stop offset="0%" style={{stopcolor:'rgb(253,137,68)',stopopacity:'1'}}></stop><stop offset="100%" style={{stopcolor:'rgb(153,75,23)',stopopacity:'1'}}></stop></linearGradient><linearGradient id="grad-red" x1="0%" y1="0%" x2="100%" y2="0%"><stop offset="0%" stop-color="#D34F48"></stop><stop offset="100%" stop-color="#772522"></stop></linearGradient></defs>
+                  <rect id="rect-grad" class="rect-gradient" fill="none" stroke="url(#grad-orange)" stroke-linecap="square" stroke-width="4" stroke-miterlimit="30" width="100%" height="100%"></rect>
+                </svg>
+              </div>
+              <div class="canvas_img-wrapper">
+                <img class="canvas_img" src="assets/img/logo/1.webp" alt="logo"/>
+              </div>
+              <div class="canvas_copy canvas_copy--left">
+                <strong class="canvas_copy_title">CROSSROADS</strong>
+                <span class="canvas_copy_details">LIQUOR</span>
+              </div>
+            </a>
+          </div>
+          </Item>
+          <Item>
+          <div class="canvas-wrapper">
+            <a href="http://www.crossroadsliquorstore.com/" class="canvas">
+              <div class="canvas_border">
+                <svg>
+                  <defs><linearGradient id="grad-orange" x1="0%" y1="0%" x2="100%" y2="0%"><stop offset="0%" style={{stopcolor:'rgb(253,137,68)',stopopacity:'1'}}></stop><stop offset="100%" style={{stopcolor:'rgb(153,75,23)',stopopacity:'1'}}></stop></linearGradient><linearGradient id="grad-red" x1="0%" y1="0%" x2="100%" y2="0%"><stop offset="0%" stop-color="#D34F48"></stop><stop offset="100%" stop-color="#772522"></stop></linearGradient></defs>
+                  <rect id="rect-grad" class="rect-gradient" fill="none" stroke="url(#grad-orange)" stroke-linecap="square" stroke-width="4" stroke-miterlimit="30" width="100%" height="100%"></rect>
+                </svg>
+              </div>
+              <div class="canvas_img-wrapper">
+                <img class="canvas_img" src="assets/img/logo/2.webp" alt="logo"/>
+              </div>
+              <div class="canvas_copy canvas_copy--left">
+                <span class="canvas_copy_subtitle">Heading</span>
+                <strong class="canvas_copy_title">Hello</strong>
+                <strong class="canvas_copy_title">World</strong>
+                <span class="canvas_copy_details">Details and stuff</span>
+              </div>
+            </a>
+          </div>
+          </Item>
+          <Item>
+          <div class="canvas-wrapper">
+            <a href="http://www.crossroadsliquorstore.com/" class="canvas">
+              <div class="canvas_border">
+                <svg>
+                  <defs><linearGradient id="grad-orange" x1="0%" y1="0%" x2="100%" y2="0%"><stop offset="0%" style={{stopcolor:'rgb(253,137,68)',stopopacity:'1'}}></stop><stop offset="100%" style={{stopcolor:'rgb(153,75,23)',stopopacity:'1'}}></stop></linearGradient><linearGradient id="grad-red" x1="0%" y1="0%" x2="100%" y2="0%"><stop offset="0%" stop-color="#D34F48"></stop><stop offset="100%" stop-color="#772522"></stop></linearGradient></defs>
+                  <rect id="rect-grad" class="rect-gradient" fill="none" stroke="url(#grad-orange)" stroke-linecap="square" stroke-width="4" stroke-miterlimit="30" width="100%" height="100%"></rect>
+                </svg>
+              </div>
+              <div class="canvas_img-wrapper">
+                <img class="canvas_img" src="assets/img/logo/3.svg" alt="logo"/>
+              </div>
+              <div class="canvas_copy canvas_copy--left">
+                <span class="canvas_copy_subtitle">Heading</span>
+                <strong class="canvas_copy_title">Hello</strong>
+                <strong class="canvas_copy_title">World</strong>
+                <span class="canvas_copy_details">Details and stuff</span>
+              </div>
+            </a>
+          </div>
+          </Item>
+          </Stack>
        
         {/* brand logo slider */}
+        <p style={{margin: "100px"}}></p>
         <BrandLogoSliderOne spaceBottomClass="pb-70" />
       </LayoutOne>
     </Fragment>
