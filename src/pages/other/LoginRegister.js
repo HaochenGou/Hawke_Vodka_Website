415823d65aa968188a9f6375577a2c96b263e607
Haochen Gou <hgou@ualberta.ca>
2022-03-17 12:34:22 -0600

    Update

--- src/pages/other/LoginRegister.js
+++ src/pages/other/LoginRegister.js
@@ -7,23 +7,17 @@ import Tab from "react-bootstrap/Tab";
 import Nav from "react-bootstrap/Nav";
 import LayoutOne from "../../layouts/LayoutOne";
 import Breadcrumb from "../../wrappers/breadcrumb/Breadcrumb";
-import {WebView} from 'react-native-webview';
-import { View } from 'react-native';
-
-
 
 const LoginRegister = ({ location }) => {
   const { pathname } = location;
- 
 
- 
   return (
     <Fragment>
       <MetaTags>
-        <title>Hawke Vodka | Login</title>
+        <title>Flone | Login</title>
         <meta
           name="description"
-          content="Compare page of flone Hawke Prohibition Distilleries."
+          content="Compare page of flone react minimalist eCommerce template."
         />
       </MetaTags>
       <BreadcrumbsItem to={process.env.PUBLIC_URL + "/"}>Home</BreadcrumbsItem>
@@ -32,14 +26,91 @@ const LoginRegister = ({ location }) => {
       </BreadcrumbsItem>
       <LayoutOne headerTop="visible">
         {/* breadcrumb */}
-        <View style={{ flex: 1, alignItems: 'flex-end' }}>
-         <WebView source={{uri: 'https://baby-x-vodka.myshopify.com/account/'}} style={{ marginTop: 20 }}/> 
-        </View>
-
         <Breadcrumb />
-
-       
-        
+        <div className="login-register-area pt-100 pb-100">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-7 col-md-12 ml-auto mr-auto">
+                <div className="login-register-wrapper">
+                  <Tab.Container defaultActiveKey="login">
+                    <Nav variant="pills" className="login-register-tab-list">
+                      <Nav.Item>
+                        <Nav.Link eventKey="login">
+                          <h4>Login</h4>
+                        </Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="register">
+                          <h4>Register</h4>
+                        </Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content>
+                      <Tab.Pane eventKey="login">
+                        <div className="login-form-container">
+                          <div className="login-register-form">
+                            <form>
+                              <input
+                                type="text"
+                                name="user-name"
+                                placeholder="Username"
+                              />
+                              <input
+                                type="password"
+                                name="user-password"
+                                placeholder="Password"
+                              />
+                              <div className="button-box">
+                                <div className="login-toggle-btn">
+                                  <input type="checkbox" />
+                                  <label className="ml-10">Remember me</label>
+                                  <Link to={process.env.PUBLIC_URL + "/"}>
+                                    Forgot Password?
+                                  </Link>
+                                </div>
+                                <button type="submit">
+                                  <span>Login</span>
+                                </button>
+                              </div>
+                            </form>
+                          </div>
+                        </div>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="register">
+                        <div className="login-form-container">
+                          <div className="login-register-form">
+                            <form>
+                              <input
+                                type="text"
+                                name="user-name"
+                                placeholder="Username"
+                              />
+                              <input
+                                type="password"
+                                name="user-password"
+                                placeholder="Password"
+                              />
+                              <input
+                                name="user-email"
+                                placeholder="Email"
+                                type="email"
+                              />
+                              <div className="button-box">
+                                <button type="submit">
+                                  <span>Register</span>
+                                </button>
+                              </div>
+                            </form>
+                          </div>
+                        </div>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </LayoutOne>
     </Fragment>
   );
@@ -48,4 +119,5 @@ const LoginRegister = ({ location }) => {
 LoginRegister.propTypes = {
   location: PropTypes.object
 };
+
 export default LoginRegister;
