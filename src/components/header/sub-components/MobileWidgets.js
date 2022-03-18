3db91325725593c5fca60b25c14a8bd83884a7d7
Haochen Gou <hgou@ualberta.ca>
2022-03-18 13:09:36 -0600

    Update

--- src/components/header/sub-components/MobileWidgets.js
+++ src/components/header/sub-components/MobileWidgets.js
@@ -8,27 +8,27 @@ const MobileWidgets = () => {
           <ul className="header-contact-info__list">
             <li>
               <i className="fa fa-phone"></i>{" "}
-              <a href="tel://7804495331">(780) 449 5331 </a>
+              <a href="tel://7804495331" target="_blank" rel="noopener noreferrer">(780) 449 5331 </a>
             </li>
             <li>
               <i className="fa fa-envelope"></i>{" "}
-              <a href="mailto:info@hawkepro.com">info@hawkepro.com</a>
+              <a href="mailto:info@hawkepro.com" target="_blank" rel="noopener noreferrer">info@hawkepro.com</a>
             </li>
           </ul>
         </div>
       </div>
       {/*Off Canvas Widget Social Start*/}
       <div className="off-canvas-widget-social">
-        <a href="//twitter.com" title="Twitter">
+        <a href="//twitter.com" target="_blank" rel="noopener noreferrer" title="Twitter">
           <i className="fa fa-twitter"></i>
         </a>
-        <a href="//instagram.com" title="Instagram">
+        <a href="//instagram.com" target="_blank" rel="noopener noreferrer" title="Instagram">
           <i className="fa fa-instagram"></i>
         </a>
-        <a href="//facebook.com" title="Facebook">
+        <a href="//facebook.com" target="_blank" rel="noopener noreferrer" title="Facebook">
           <i className="fa fa-facebook"></i>
         </a>
-        <a href="//pinterest.com" title="Pinterest">
+        <a href="//pinterest.com" target="_blank" rel="noopener noreferrer" title="Pinterest">
           <i className="fa fa-pinterest"></i>
         </a>
       </div>
