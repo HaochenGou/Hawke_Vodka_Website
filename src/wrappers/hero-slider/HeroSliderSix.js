7c9b46bd1053399122f27e1b0dbad9affd291825
Haochen Gou <hgou@ualberta.ca>
2022-04-08 10:01:22 -0600

    Update

--- src/wrappers/hero-slider/HeroSliderSix.js
+++ src/wrappers/hero-slider/HeroSliderSix.js
@@ -1,35 +1,16 @@
 import React from "react";
 import Swiper from "react-id-swiper";
-import heroSliderData from "../../data/hero-sliders/hero-slider-six.json";
-import HeroSliderSixSingle from "../../components/hero-slider/HeroSliderSixSingle.js";
+
 
 const HeroSliderSix = () => {
-  const params = {
-    effect: "fade",
-    loop: false,
-    speed: 1000,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    watchSlidesVisibility: true,
-  };
 
   return (
-    <div className="slider-area">
-      <div className="slider-active nav-style-1">
-        <Swiper {...params}>
-          {heroSliderData &&
-            heroSliderData.map((single, key) => {
-              return (
-                <HeroSliderSixSingle
-                  sliderClassName="swiper-slide"
-                  data={single}
-                  key={key}
-                />
-              );
-            })}
-        </Swiper>
+    <div className="container">
+        <div className="row">
+        <video autoPlay muted loop > 
+          <source src="/assets/img/add.webm" type="video/webm" alt></source>
+        </video>
+        
       </div>
     </div>
   );
