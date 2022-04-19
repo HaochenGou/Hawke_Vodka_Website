f319f756001219c0315ed42837f59811ae3f1016
Haochen Gou <hgou@ualberta.ca>
2022-04-19 16:21:09 -0600

    Update

--- src/pages/other/Friend.js
+++ src/pages/other/Friend.js
@@ -10,6 +10,7 @@ import { Stack } from "@mui/material";
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import FriendMap from "./Map";
+import { padding } from "@mui/system";
 
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -230,6 +231,52 @@ const Friend = ({ location }) => {
                 <span class="canvas_copy_details">EXCHANGE</span>
               </div>
             </a>
+          </div>
+          </Item>
+        </Stack>
+        <Stack direction={{ xs: 'column', sm: 'row' }} spacing={{ xs: 1, sm: 2, md: 8 }}>
+        <p className="canvas_front"></p>
+          <Item>
+          <div class="canvas-wrapper">
+            <a href="https://www.liquordepot.ca/" class="canvas" rel="noopener noreferrer" target="_blank">
+              <div class="canvas_border">
+                <svg>
+                  <defs><linearGradient id="grad-orange" x1="0%" y1="0%" x2="100%" y2="0%"><stop offset="0%" style={{stopcolor:'rgb(253,137,68)',stopopacity:'1'}}></stop><stop offset="100%" style={{stopcolor:'rgb(153,75,23)',stopopacity:'1'}}></stop></linearGradient><linearGradient id="grad-red" x1="0%" y1="0%" x2="100%" y2="0%"><stop offset="0%" stop-color="#D34F48"></stop><stop offset="100%" stop-color="#772522"></stop></linearGradient></defs>
+                  <rect id="rect-grad" class="rect-gradient" fill="none" stroke="url(#grad-orange)" stroke-linecap="square" stroke-width="4" stroke-miterlimit="30" width="100%" height="100%"></rect>
+                </svg>
+              </div>
+              <div class="canvas_img-wrapper">
+                <img class="canvas_img" src="assets/img/logo/7.webp" alt="logo"/>
+              </div>
+              <div class="canvas_copy canvas_copy--left">
+                <strong class="canvas_copy_title">LIQUOR</strong>
+                <span class="canvas_copy_details">DEPOT</span>
+              </div>
+            </a>
+          </div>
+          </Item>
+          <Item>
+          <div class="canvas-wrapper">
+            <a href="https://www.wineandbeyond.ca/" class="canvas" rel="noopener noreferrer" target="_blank">
+              <div class="canvas_border">
+                <svg>
+                  <defs><linearGradient id="grad-orange" x1="0%" y1="0%" x2="100%" y2="0%"><stop offset="0%" style={{stopcolor:'rgb(253,137,68)',stopopacity:'1'}}></stop><stop offset="100%" style={{stopcolor:'rgb(153,75,23)',stopopacity:'1'}}></stop></linearGradient><linearGradient id="grad-red" x1="0%" y1="0%" x2="100%" y2="0%"><stop offset="0%" stop-color="#D34F48"></stop><stop offset="100%" stop-color="#772522"></stop></linearGradient></defs>
+                  <rect id="rect-grad" class="rect-gradient" fill="none" stroke="url(#grad-orange)" stroke-linecap="square" stroke-width="4" stroke-miterlimit="30" width="100%" height="100%"></rect>
+                </svg>
+              </div>
+              <div class="canvas_img-wrapper">
+                <img class="canvas_img" src="assets/img/logo/8.webp" alt="logo"/>
+              </div>
+              <div class="canvas_copy canvas_copy--left">
+                <strong class="canvas_copy_title">WINE AND BEYOND</strong>
+                <span class="canvas_copy_details">SPIRITS BEER & MORE</span>
+              </div>
+            </a>
+          </div>
+          </Item>
+          <Item>
+            <div style={{fontSize:'20px',alignContent:'center'}}>
+          <a href="mailto:baselineliquor@shaw.ca" class="canvas" rel="noopener noreferrer" target="_blank">PRAIRIE LAND LIQUOR STORE</a>
           </div>
           </Item>
         </Stack>
