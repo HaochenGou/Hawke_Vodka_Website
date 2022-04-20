6a54c2b56b8f4b3a622e12652dff7c076b159a2f
Haochen Gou <hgou@ualberta.ca>
2022-04-20 14:49:44 -0600

    Update

--- src/components/hero-slider/HeroSliderThreeSingle.js
+++ src/components/hero-slider/HeroSliderThreeSingle.js
@@ -29,9 +29,37 @@ const HeroSliderThreeSingle = ({ data, sliderClass }) => {
               height="100%"
             /> 
             </div>  
-            <div className="mobilebg" id="content-mobile2" >
-            <div className="container">
+            <div className="container" id="content-desktop2">
           
+        <div className="row align-items-left slider-h9-mrg">
+        <div className="col-lg-6 col-md-6 col-12 col-sm-6">
+            <div className="slider-singleimg-hm9 slider-animated-1">
+              <img
+                className="animated"
+                src={process.env.PUBLIC_URL + data.image}
+                alt=""
+              />
+            </div>
+          </div>
+          <div className="col-lg-6 col-md-6 col-12 col-sm-6">
+            <div className="slider-content-7 slider-animated-1">
+              <h3 className="animated">{data.title}</h3>
+              <h1 className="animated">{data.subtitle}</h1>
+              <div className="slider-btn-9 btn-hover">
+                <Link
+                  className="animated"
+                  to={process.env.PUBLIC_URL + data.url}
+                >
+                  BUY NOW
+                </Link>
+              </div>
+            </div>
+          </div>      
+        </div>
+      </div>
+      <div className="mobilebg" id="content-mobile2" >
+      <p style={{margin: "10px"}}></p>
+      <div className="container">
         <div className="row align-items-left slider-h9-mrg">
         <div className="col-lg-6 col-md-6 col-12 col-sm-6">
             <div className="slider-singleimg-hm9 slider-animated-1">
