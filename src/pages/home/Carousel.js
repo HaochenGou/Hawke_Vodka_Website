be1766f1ac9b367551d85470be43b00ef540c9cc
Haochen Gou <hgou@ualberta.ca>
2022-05-05 09:41:43 -0600

    Update

--- src/pages/home/Carousel.js
+++ src/pages/home/Carousel.js
@@ -19,27 +19,27 @@ export default class Example extends Component {
 	slides = [
 	  {
 		key: uuidv4(),
-		content:<a href={process.env.PUBLIC_URL +"/product/" + 41435507064983}><img width="380" height="400"src='/assets/img/Product1.webp' alt="1"/></a>
+		content:<a href={process.env.PUBLIC_URL +"/product/" + 41435507064983}><img width="380" height="400"src='/assets/img/Product1.webp' alt="1" loading="lazy"/></a>
 	  },
 	  {
 		key: uuidv4(),
-		content: <a href={process.env.PUBLIC_URL +"/product/" + 37852889809047}><img width="380" height="400" src='/assets/img/Product2.webp' alt="2"/></a>
+		content: <a href={process.env.PUBLIC_URL +"/product/" + 37852889809047}><img width="380" height="400" src='/assets/img/Product2.webp' alt="2"loading="lazy"/></a>
 	  },
 	  {
 		key: uuidv4(),
-		content:  <a href={process.env.PUBLIC_URL +"/product/" + 41435485995159}><img width="380" height="400" src='/assets/img/Product3.webp' alt="3"/></a>
+		content:  <a href={process.env.PUBLIC_URL +"/product/" + 41435485995159}><img width="380" height="400" src='/assets/img/Product3.webp' alt="3" loading="lazy"/></a>
 	  },
 	  {
 		key: uuidv4(),
-		content: <a href={process.env.PUBLIC_URL +"/product/" +41873399251095}><img width="380" height="400"src='/assets/img/Product4.webp' alt="4"/></a>
+		content: <a href={process.env.PUBLIC_URL +"/product/" +41873399251095}><img width="380" height="400"src='/assets/img/Product4.webp' alt="4" loading="lazy"/></a>
 	  },
 	  {
 		key: uuidv4(),
-		content:  <a href={process.env.PUBLIC_URL +"/product/" + 41907321995415}><img width="380" height="400" src='/assets/img/Product5.webp' alt="5"/></a>
+		content:  <a href={process.env.PUBLIC_URL +"/product/" + 41907321995415}><img width="380" height="400" src='/assets/img/Product5.webp' alt="5" loading="lazy"/></a>
 	  },
 	  {
 		key: uuidv4(),
-		content:  <a href={process.env.PUBLIC_URL +"/product/" + 41981615636631}><img width="380" height="400" src='/assets/img/Product6.webp' alt="6"/></a>
+		content:  <a href={process.env.PUBLIC_URL +"/product/" + 41981615636631}><img width="380" height="400" src='/assets/img/Product6.webp' alt="6" loading="lazy"/></a>
 	  },
 	 
 	].map((slide, index) => {
